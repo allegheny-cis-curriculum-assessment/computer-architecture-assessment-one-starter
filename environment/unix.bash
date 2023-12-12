6c4611c58f1f18363e182bb484ccf4e608f2c425
dluman <dluman@allegheny.edu>
2023-12-12 07:53:23 -0500

    Removing former environment build scripts

--- environment/unix.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-cwd=$(pwd)
-mkdir /VSARM
-cd /VSARM
-sudo apt update
-sudo apt-get install git -y
-git clone -b master --recurse-submodules https://github.com/raspberrypi/pico-sdk.git
-cd $cwd
